8be47a09f023c6318e3b7b21f0eb3c3596d9588f
Philippe Schuller <ffvpor@orange.fr>
2020-12-01 18:02:45 +0100

    ci(template): add missing code in set-dev-back.sh

--- scripts/set-ffs-back.sh
+++ scripts/set-ffs-back.sh
@@ -10,12 +10,13 @@ if [ ! -d ./femto-full-stack ] ; then
     exit 1
 fi
 
-if [ $(uname -s) = "Linux"]; then
+if [ $(uname -s) = "Linux" ] ; then
   echo "Remove __pycache__ files"
-  find ./develop-femto-full-stack/ -type -d -name '__pycache__' -exec rm -r {} \+
+  find ./develop-femto-full-stack/ -type d -name __pycache__ -prune -exec rm -rf {} +
+fi
 
 echo "Remove all files in femto-full-stack"
 rm -rf ./femto-full-stack/\{\{cookiecutter.project_blueprint\}\}/*
 
 echo "Sync files from develop-femto-full-stack to femto-full-stack"
-rsync -a --exclude=node_modules --exclude='.venv'
+rsync -a --exclude=node_modules --exclude='.venv' ./develop-femto-full-stack/* ./femto-full-stack/\{\{cookiecutter.project_blueprint\}\}/
